b420f6209d0013d0cfb21db0043623c607ebfb46
dmitro92 <kopalidi92@gmail.com>
2021-01-09 17:46:49 +0600

    task 3 complete

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ let movieList = document.querySelector(".promo__interactive-list");
 let buttonOne = document.querySelector("#buttonOne");
 let addInput = document.querySelector("#add_input");
 let NimberOfMovies = movieDB.movies.length; 
+let deleteButton = document.querySelectorAll(".delete");
 
 
 promoAdvImage.forEach(element => { 
@@ -39,11 +40,13 @@ movieDB.movies.forEach((film, i) => {
 
 buttonOne.addEventListener('click', (e) => {
     event.preventDefault();
+    if (!addInput.value) return;
+
     movieList.innerHTML += `
         <li class="promo__interactive-item"> ${NimberOfMovies + 1}.  ${addInput.value.slice(1, 8) + '...'} 
             <div class="delete"></div>
         </li>
-    `; // добавил slice в addInput для 2 задания
+    `; 
 
     movieDB.movies += `,${addInput.value}`;
     console.log(movieDB.movies);
@@ -54,5 +57,11 @@ buttonOne.addEventListener('click', (e) => {
     addInput.value = '';
 });
 
+movieList.addEventListener('click', (e) => { // Третье задание выполнено
+    
+    if (e.target.className == 'delete') {
+        e.target.parentElement.remove();
+    }
+});
 
 
