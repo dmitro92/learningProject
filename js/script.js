4168f7c263100a11ed6edf22783a3c398259ff8b
dmitro92 <kopalidi92@gmail.com>
2021-01-10 01:18:50 +0600

    teachersVariation

--- js/script.js
+++ js/script.js
@@ -8,66 +8,85 @@ const movieDB = {
         "Скотт Пилигрим против..."                                                                                           
     ]
 };
-const promoAdvImage = document.querySelectorAll(".promo__adv img");
+
+const adv = document.querySelectorAll(".promo__adv img");
 const promoGenre = document.querySelector(".promo__genre");
 const promoBg = document.querySelector(".promo__bg");
 let movieList = document.querySelector(".promo__interactive-list");
-let buttonOne = document.querySelector("#buttonOne");
-let addInput = document.querySelector("#add_input");
-let NimberOfMovies = movieDB.movies.length; 
-let deleteButton = document.querySelectorAll(".delete");
-let favoriteCheckbox = document.querySelector(".favorite");
+let addForm = document.querySelector("form.add"); //ролучаем всю форму с классом add
+    addInput = addForm.querySelector(".adding__input"); //получаем инпут
+    checkbox = addForm.querySelector("[type='checkbox']"); //ролучаем чекбокс
+
+const deleteAdv = (arr) => {
+    arr.forEach(item => { 
+        item.remove();
+    });
+};
+
+const makeChanges = () => {
+    promoGenre.textContent = "Драма"; 
+
+    promoBg.style.backgroundImage = 'url("img/bg.jpg")'; 
+};
+
+const sortArr = (arr) => {
+    arr.sort();
+};
 
 
-promoAdvImage.forEach(element => { 
-    element.remove();
-});
 
-promoGenre.textContent = "Драма"; 
 
-promoBg.style.backgroundImage = 'url("img/bg.jpg")'; 
 
-movieList.innerHTML = ''; 
 
-movieDB.movies.sort(); 
 
-movieDB.movies.forEach((film, i) => { 
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}. ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
 
-buttonOne.addEventListener('click', (e) => {
+addForm.addEventListener('submit', (e) => { //submit вместо click
     event.preventDefault();
-    if (!addInput.value) return;
 
-    movieList.innerHTML += `
-        <li class="promo__interactive-item"> ${NimberOfMovies + 1}.  ${addInput.value.slice(1, 8) + '...'} 
-            <div class="delete"></div>
-        </li>
-    `; 
+    let newFilm = addInput.value;
+    const favorite = checkbox.ckecked;
 
-    movieDB.movies += `,${addInput.value}`;
-    console.log(movieDB.movies);
-    
+    if (newFilm) { // чтобы не делать вложенность можно заменить на одну строчку - if (!addInput.value) return;
 
-    NimberOfMovies += 1;
-  
-    addInput.value = '';
+        if (newFilm.length >21) {
+            newFilm = `${newFilm.substring(0, 22)}...`; // см. мой вариант
+        }
+        
+        if (favorite) { 
+            console.log('Добавляем любимый фильм');
+        }
 
-    if (favoriteCheckbox.checked == true) { //Четвёртое задание выполнено
-        console.log('Добавляем любимый фильм');
+        movieDB.movies.push(newFilm); //push это метод который добавляет через запятую значение в виде строки
+        sortArr(movieDB.movies);
+
+        createMovieList(movieDB.movies, movieList);
     }
+
+    event.target.reset(); // либо addForm.reset(); ресет - метод который очищает инпут
 });
 
-movieList.addEventListener('click', (e) => { 
-    
-    if (e.target.className == 'delete') {
-        e.target.parentElement.remove();
-    }
-});
+function createMovieList(films, parent) {
+    parent.innerHTML = ''; 
+    sortArr(films);
 
+    films.forEach((film, i) => { 
+        parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1}. ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
 
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1); // splice - метод, который удаляет элемент из массива
 
+            createMovieList(films, parent); // применяем рекурсию - использование функции внутри себя
+        });
+    }); 
+}
+
+createMovieList(movieDB.movies, movieList);
+deleteAdv(adv);
+makeChanges();
